3527b96525adb6746edf01962757a5c6057d7c4c
Terence Chiu <terence_chiu@m018452.aw.org>
2022-10-26 14:29:09 -0700

    Wikipedia

---
+++ wikipedia.py
@@ -0,0 +1,43 @@
+def isHyperlink(s):
+
+    return True
+    return False
+
+class Article:
+    def __init__(self, t, title):
+        self.text = t
+        self.title= title
+
+    def getText(self):
+        return self.text
+    def getTitle(self):
+        return self.title
+
+    
+
+    def compare(self, a):
+        self.title = temp
+        i = a.getText().index(temp)
+        s = []
+        
+        if i = -1:
+            return s.randomChoice()
+        else if temp.isHyperlink():
+            return self.t
+            
+def main():
+    text = ""
+    title = ""
+    endText = ""
+    endTitle = ""
+    
+    begin = Article(text, title)
+
+    end = Article(endText, endTitle)
+
+    begin.compare
+
+if __name__ == "__main__":
+    main()
+        
+        
